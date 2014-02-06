d09b12022fb86e76b8c97894e658fae9dc5f7a83
shirilad <shirilad@gmail.com>
2014-02-07 00:58:28 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DonateForwardStore::Application.config.secret_key_base = '5e2ee66ede1ea664bc796a96c942e8e0fd996eb0766a6e55692ecd9b5a73299287fc9729221c8e7cd7aa8ee50df967d80b89fcd24ad25ca9f0ae8bec073d043a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DonateForwardStore::Application.config.session_store :cookie_store, key: '_DonateForwardStore_session'
